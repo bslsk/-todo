677b422b9317758c5be104074cb2d11e916bb4fd
Basil Muthalaly <Basilisk@Georges-Air.attlocal.net>
2015-12-21 22:33:46 -0600

    Initial Commit

---
+++ Todo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Todo
+//
+//  Created by Basil Muthalaly on 12/21/15.
+//  Copyright (c) 2015 Basil Muthalaly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Todo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Basil Muthalaly on 12/21/15.
+//  Copyright (c) 2015 Basil Muthalaly. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
